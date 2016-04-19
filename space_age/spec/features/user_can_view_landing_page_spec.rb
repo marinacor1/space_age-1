428f9f24b8940759801b498c762c0cfdd6ff5e4d
Jbern16 <jbern16@gmail.com>
2016-04-19 10:42:30 -0600

    add database setup

--- space_age/spec/features/user_can_view_landing_page_spec.rb
+++ space_age/spec/features/user_can_view_landing_page_spec.rb
@@ -3,14 +3,15 @@ require "rails_helper"
 RSpec.describe "user can land on root path" do
   scenario "user sees basic layout of page" do
     visit_root_path
-    expect(page).to have_content "Browse"
+
     expect(page).to have_content "Logo"
-    expect(page).to have_content "Cart"
-    
-    within("nav_bar") do
-      expect(page).to have_link "Cart", href: cart_path
-      expect(page).to have_content "Logo"
-    end
+
+    expect(page).to have_link "Explore", href: packages_path #items index
+      
+    # within("nav_bar") do
+    #   expect(page).to have_link "Cart", href: cart_path
+    #   expect(page).to have_content "Logo"
+    # end
 
   end
 end
