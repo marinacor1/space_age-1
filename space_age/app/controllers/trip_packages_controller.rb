78ae03fcd968454cc9ca8d14e854552b83e9eeb6
marinacor1 <marinaspam1234@gmail.com>
2016-04-19 15:40:28 -0600

    Adding cart feature test and controller class

---
+++ space_age/app/controllers/trip_packages_controller.rb
@@ -0,0 +1,13 @@
+class TripPackagesController < ApplicationController
+  def create
+    package = Package.find(params[:package_id])
+
+    @trip.add_package(package)
+
+    session[:trip] = @trip.contents
+
+    flash[:notice] = "You have added #{package.title} to your trip. Your current trip costs: #{package.price}."
+    redirect_to package_path(package)
+  end
+
+end
---
+++ space_age/spec/features/user_can_add_packages_to_trip_spec.rb
@@ -0,0 +1,14 @@
+require 'rails_helper'
+
+RSpec.feature "user can add package to trip" do
+  scenario "user can see package in trip (cart)" do
+    package = create(:package)
+
+    visit package_path(package)
+
+    click_on "Add Package to Trip"
+
+    expect(page).to have_content ("You have added #{package.title} to your trip.")
+    expect(page).to have_content "Your current trip costs: #{package.price}."
+  end
+end
