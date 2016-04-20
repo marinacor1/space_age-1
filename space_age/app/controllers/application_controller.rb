c6c9696300fb33e513a1a935d2e6cf04eef5b5a1
Jeneve Parrish <jeneve.parrish@gmail.com>
2016-04-20 17:02:38 -0600

    created a current_user

--- space_age/app/controllers/application_controller.rb
+++ space_age/app/controllers/application_controller.rb
@@ -11,4 +11,8 @@ class ApplicationController < ActionController::Base
     @trip = Trip.new(session[:trip])
   end
 
+  def current_user
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
+
 end
