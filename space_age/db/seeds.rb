51f7c41ec0ce48f8959ac0a9f83f051f6afd483f
Jeneve Parrish <jeneve.parrish@gmail.com>
2016-04-19 21:24:49 -0600

    Add preliminary index styling   -adds seed data and more images so that the view I styled was populated     *to use my seed data, accept the pull request, pull then run rake db:drop db:create db:migrate db:seed   -adds an orbit-slide feature to the top of the index page, the images are hard-coded   -adds styling to what was already displayed on the index page   -adds height and width to the "thumbnail" class to make images uniform on the page
     	to do:
    	  -not sure if the action on the orbit-slide is working properly
              -need to change the css on the "thumbnail" class to crop the image rather than distort it

--- space_age/db/seeds.rb
+++ space_age/db/seeds.rb
@@ -1,7 +1,105 @@
 
   Destination.create(planet: "Mars")
+
+
+  package = Package.create(title: "Mars Adventure",
+                     description: Faker::Lorem.paragraph,
+                           price: rand(3..9) + 1000,
+                           image: "planet_mars_1.jpeg",
+                  destination_id: 1)
+  puts "Created #{package.title}, price: #{package.price}"
+  package = Package.create(title: "Mars Family",
+                     description: Faker::Lorem.paragraph,
+                           price: rand(3..9) + 1000,
+                           image: "planet_mars_2.jpeg",
+                  destination_id: 1)
+  puts "Created #{package.title}, price: #{package.price}"
+  package = Package.create(title: "Mars Luxury",
+                     description: Faker::Lorem.paragraph,
+                           price: rand(3..9) + 1000,
+                           image: "planet_mars_3.jpeg",
+                  destination_id: 1)
+  puts "Created #{package.title}, price: #{package.price}"
+  package = Package.create(title: "Mars Weekend",
+                     description: Faker::Lorem.paragraph,
+                           price: rand(3..9) + 1000,
+                           image: "planet_mars_4.jpeg",
+                  destination_id: 1)
+  puts "Created #{package.title}, price: #{package.price}"
+  package = Package.create(title: "Mars Service Trip",
+                     description: Faker::Lorem.paragraph,
+                           price: rand(3..9) + 1000,
+                           image: "planet_mars_5.jpeg",
+                  destination_id: 1)
+  puts "Created #{package.title}, price: #{package.price}"
+
+
   Destination.create(planet: "Venus")
 
 
-  Package.create(title: "basic", destination_id: 1)
-  Package.create(title: "basic", destination_id: 2)
+  package = Package.create(title: "Venus Adventure",
+                     description: Faker::Lorem.paragraph,
+                           price: rand(3..9) + 1000,
+                           image: "planet_venus_1.jpeg",
+                  destination_id: 2)
+  puts "Created #{package.title}, price: #{package.price}"
+  package = Package.create(title: "Venus Family",
+                     description: Faker::Lorem.paragraph,
+                           price: rand(3..9) + 1000,
+                           image: "planet_venus_2.jpeg",
+                  destination_id: 2)
+  puts "Created #{package.title}, price: #{package.price}"
+  package = Package.create(title: "Venus Luxury",
+                     description: Faker::Lorem.paragraph,
+                           price: rand(3..9) + 1000,
+                           image: "planet_venus_3.jpeg",
+                  destination_id: 2)
+  puts "Created #{package.title}, price: #{package.price}"
+  package = Package.create(title: "Venus Weekend",
+                     description: Faker::Lorem.paragraph,
+                           price: rand(3..9) + 1000,
+                           image: "planet_venus_4.jpeg",
+                  destination_id: 2)
+  puts "Created #{package.title}, price: #{package.price}"
+  package = Package.create(title: "Venus Service Trip",
+                     description: Faker::Lorem.paragraph,
+                           price: rand(3..9) + 1000,
+                           image: "planet_venus_5.jpeg",
+                  destination_id: 2)
+  puts "Created #{package.title}, price: #{package.price}"
+
+
+
+  Destination.create(planet: "Moon")
+
+
+  package = Package.create(title: "Moon Adventure",
+                     description: Faker::Lorem.paragraph,
+                           price: rand(3..9) + 1000,
+                           image: "planet_moon_1.jpeg",
+                  destination_id: 3)
+  puts "Created #{package.title}, price: #{package.price}"
+  package = Package.create(title: "Moon Family",
+                     description: Faker::Lorem.paragraph,
+                           price: rand(3..9) + 1000,
+                           image: "planet_moon_2.jpeg",
+                  destination_id: 3)
+  puts "Created #{package.title}, price: #{package.price}"
+  package = Package.create(title: "Moon Luxury",
+                     description: Faker::Lorem.paragraph,
+                           price: rand(3..9) + 1000,
+                           image: "planet_moon_3.jpeg",
+                  destination_id: 3)
+  puts "Created #{package.title}, price: #{package.price}"
+  package = Package.create(title: "Moon Weekend",
+                     description: Faker::Lorem.paragraph,
+                           price: rand(3..9) + 1000,
+                           image: "planet_moon_4.jpeg",
+                  destination_id: 3)
+  puts "Created #{package.title}, price: #{package.price}"
+  package = Package.create(title: "Moon Service Trip",
+                     description: Faker::Lorem.paragraph,
+                           price: rand(3..9) + 1000,
+                           image: "planet_moon_5.jpeg",
+                  destination_id: 3)
+  puts "Created #{package.title}, price: #{package.price}"
