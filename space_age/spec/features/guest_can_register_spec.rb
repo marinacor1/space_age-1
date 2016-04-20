12de0b4c1c83e097429c145762ac8d28a7684f7e
Jeneve Parrish <jeneve.parrish@gmail.com>
2016-04-20 15:34:29 -0600

    adds a test for guest registers

---
+++ space_age/spec/features/guest_can_register_spec.rb
@@ -0,0 +1,46 @@
+# As a visitor
+# When I visit "/"
+# Then I should see a link for "Login"
+# And when I click "Login"
+# And I should be on the "/login" page
+# I should see a place to insert my credentials to login
+# And I should see a link to "Create Account"
+#
+# As a visitor
+# When I visit "/login
+# And when I click link "Create Account"
+# And I fill in my desired credentials
+# And I submit my information
+# Then my current page should be "/dashboard"
+# And I should see a message in the navbar that says "Logged in as SOME_USER"
+# And I should see my profile information
+# And I should not see a link for "Login"
+# And I should see a link for "Logout"
+
+require 'rails_helper'
+
+RSpec.feature "guest can register" do
+  context "with valid inputs" do
+    scenario "they see a link to log in" do
+      visit root_path
+
+      click_on "Login"
+
+      expect(current_path).to eq login_path
+
+      click_on "Create Account"
+
+      expect(current_path).to eq signup_path
+
+      within 'form' do
+        fill_in "Username", with: "User"
+        fill_in "Email", with: "email"
+        fill_in "Password", with: "password"
+        fill_in "Password confirmation", with: "password"
+        click_button "Create Account"
+      end
+
+      expect(current_path).to eq login_path        
+    end
+  end
+end
