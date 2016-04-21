83fa386e9dd9c39a504dad9808cac133398148c8
marinacor1 <marinaspam1234@gmail.com>
2016-04-20 18:31:40 -0600

    Adding route for logging in

--- space_age/config/routes.rb
+++ space_age/config/routes.rb
@@ -1,11 +1,9 @@
 Rails.application.routes.draw do
   root to: "welcome#show"
   get '/login', to: "sessions#new"
-
+  post '/login', to: "sessions#create"
   get '/signup', to: "users#new"
-
   get '/dashboard', to: "users#show"
-
   delete '/logout', to: 'sessions#destroy'
 
   resources :users, except: [:new, :show]
@@ -13,11 +11,7 @@ Rails.application.routes.draw do
   get '/trip', to: "trip_packages#show"
 
   resources :packages, only: [ :index, :show ]
-
   resources :trip_packages, only: [ :create, :destroy, :update ]
 
-
   get '/:planet', to: "destinations#show", as: :destination
-
-
 end
