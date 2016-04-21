1c5433f831b54c0c06a66747389537153cad84d9
Jeneve Parrish <jeneve.parrish@gmail.com>
2016-04-20 21:23:25 -0600

    Add user can log out functionality 	-adds destroy method to sessions controller 	-logged out user can see a link to log in 	-they can't see a link to log out

--- space_age/spec/features/guest_user_can_see_cart_spec.rb
+++ space_age/spec/features/guest_user_can_see_cart_spec.rb
@@ -6,6 +6,7 @@
 # After I create an account
 # And I visit "/cart
 # Then I should see all of the data that was there when I was not logged in
+
 # When I click "Logout"
 # Then I should see see "Login"
 # And I should not see "Logout"
@@ -21,10 +22,43 @@ RSpec.feature "guest user can see cart" do
       click_on "Add Package to Trip"
 
       visit '/trip'
-      save_and_open_page
 
       expect(page).to_not have_link("Checkout", checkout_path)
       expect(page).to have_link("Login or Create Account to Purchase", login_path)
+    end
+  end
+
+  context "a guest user returns to the cart after registering" do
+    scenario "they see all of the data that was there before they left to register" do
+      package = create(:package)
+
+      visit package_path(package)
+
+      click_on "Add Package to Trip"
+
+      visit '/trip'
+
+      click_on "Login or Create Account to Purchase"
+
+      click_on "Create Account"
+
+      within ".form" do
+        fill_in "Username", with: "User"
+        fill_in "Email", with: "email"
+        fill_in "Password", with: "password"
+        fill_in "Password confirmation", with: "password"
+        click_button "Create Account"
+      end
+
+      visit '/trip'
+
+      expect(page).to have_content package.title
+      expect(page).to have_content package.description
+      expect(page).to have_content package.price.to_s
+      expect(page).to have_xpath("//img[@src=\"#{package.image}\"]")
+
+      expect(page).to have_link("Checkout", checkout_path)
+      expect(page).to_not have_link("Login or Create Account to Purchase", login_path)
     end
   end
 end
---
+++ space_age/spec/features/user_can_log_out_spec.rb
@@ -0,0 +1,25 @@
+# When I click "Logout"
+# Then I should see see "Login"
+# And I should not see "Logout"
+require 'rails_helper'
+
+RSpec.feature "user can log out" do
+  scenario "they see a link to log in and not a link to log out" do
+    user = create(:user)
+
+    visit login_path
+
+    within ".login_form" do
+      fill_in "Username", with: "User"
+      fill_in "Password", with: "password"
+      click_on "Sign In"
+    end
+
+    visit '/cart'
+
+    click_on "Logout"
+
+    expect(page).to have_content("Login")
+    expect(page).to_not have_content("Logout")
+  end
+end
