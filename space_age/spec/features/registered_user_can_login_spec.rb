d939ca2cabaf9b4a16493607aec25863389a247f
marinacor1 <marinaspam1234@gmail.com>
2016-04-20 18:57:31 -0600

    Incorrect username will not let you login spec passing

--- space_age/spec/features/registered_user_can_login_spec.rb
+++ space_age/spec/features/registered_user_can_login_spec.rb
@@ -39,4 +39,24 @@ RSpec.feature "registered user can log in" do
       expect(page).to_not have_link "Logout", logout_path
     end
   end
+
+  context "with invalid username" do
+    scenario "they see an error message" do
+      user = create(:user)
+
+      visit '/login'
+      click_on "Login"
+
+      expect(current_path).to eq(login_path)
+      within ".login_form" do
+        fill_in "Username", with: "Userzz"
+        fill_in "Password", with: "password"
+        click_on "Sign In"
+      end
+      expect(current_path).to eq(login_path)
+      expect(page).to have_content "Incorrect email/password combination."
+      expect(page).to_not have_link "Logout", logout_path
+    end
+  end
+
 end
