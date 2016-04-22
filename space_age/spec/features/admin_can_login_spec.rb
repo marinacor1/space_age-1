c601fe6fb2e86427cc4a1c8292066174b64999af
marinacor1 <marinaspam1234@gmail.com>
2016-04-21 18:12:53 -0600

    Sad path guest cannot see admin dashboard passing

--- space_age/spec/features/admin_can_login_spec.rb
+++ space_age/spec/features/admin_can_login_spec.rb
@@ -6,14 +6,21 @@ RSpec.feature "admin can login" do
     admin = User.create(username: "adminor", email: "emailzzz", password: "password", password_confirmation: "password", role: 1)
 
     admin_login
+
     expect(current_path).to eq '/admin/dashboard'
     expect(page).to have_content "Admin Dashboard"
   end
 
-  it "when non admin tries to access admin dashboard gets error" do
+  it "when non admin registered user tries to access admin dashboard gets error" do
      user = create(:user)
 
     ApplicationController.any_instance.stubs(:current_user).returns(user)
+
+    visit admin_dashboard_path
+    expect(page).to have_content "The page you were looking for doesn't exist."
+  end
+
+  it "when guest tries to access admin dashboard gets error" do
 
     visit admin_dashboard_path
     expect(page).to have_content "The page you were looking for doesn't exist."
