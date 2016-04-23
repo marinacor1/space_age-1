edde7f2c9a912a6be62a89114ca703abb4305316
marinacor1 <marinaspam1234@gmail.com>
2016-04-22 20:39:48 -0600

    Adding test for user delete

---
+++ space_age/spec/features/user_can_delete_account_spec.rb
@@ -0,0 +1,60 @@
+require 'rails_helper'
+
+RSpec.feature "registered user can delete account" do
+  include FeatureHelper
+  context "with valid params" do
+    scenario "they see the packages page" do
+      user = create(:user)
+
+      user_login
+
+      expect(current_path).to eq(packages_path)
+      visit '/dashboard'
+      click_on "Delete My Account"
+
+      expect(current_path).to eq(root_path)
+      expect(page).to not_have_content "Andrew"
+      expect(page).not_to have_content "email@email.com"
+      expect(page).to have_content "Account Successfully Deleted"
+    end
+  end
+
+  xcontext "with invalid params as an admin" do
+    xscenario "they see an error message" do
+      user = create(:user)
+
+      visit '/login'
+      click_on "Login"
+
+      expect(current_path).to eq(login_path)
+      within ".login_form" do
+        fill_in "Username", with: "User"
+        fill_in "Password", with: "password1"
+        click_on "Sign In"
+      end
+      expect(current_path).to eq(login_path)
+      expect(page).to have_content "Incorrect email/password combination."
+      expect(page).to_not have_link "Logout", logout_path
+    end
+  end
+
+  xcontext "as a guest" do
+    xscenario "they see an error message" do
+      user = create(:user)
+
+      visit '/login'
+      click_on "Login"
+
+      expect(current_path).to eq(login_path)
+      within ".login_form" do
+        fill_in "Username", with: "Userzz"
+        fill_in "Password", with: "password"
+        click_on "Sign In"
+      end
+      expect(current_path).to eq(login_path)
+      expect(page).to have_content "Incorrect email/password combination."
+      expect(page).to_not have_link "Logout", logout_path
+    end
+  end
+
+end
