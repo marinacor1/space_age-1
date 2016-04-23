0d157c47a4a494f707874164d5671e331f710519
marinacor1 <marinaspam1234@gmail.com>
2016-04-23 17:02:16 -0600

    User module method to refactore user can login feature spec

--- space_age/spec/features/registered_user_can_login_spec.rb
+++ space_age/spec/features/registered_user_can_login_spec.rb
@@ -1,20 +1,12 @@
 require 'rails_helper'
 
 RSpec.feature "registered user can log in" do
+  include FeatureHelper
   context "with valid inputs" do
     scenario "they see a link to log in" do
-      user = create(:user)
+      @user = create(:user)
 
-      visit '/login'
-      click_on "Login"
-
-      expect(current_path).to eq(login_path)
-
-      within ".login_form" do
-        fill_in "Username", with: user.username 
-        fill_in "Password", with: "password"
-        click_on "Sign In"
-      end
+      user_login
 
       expect(current_path).to eq(packages_path)
       expect(page).to_not have_content "Login"
