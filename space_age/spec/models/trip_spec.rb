731c464e38d4da8b68da7d7d9d32b41dd78cd1fd
Jbern16 <jbern16@gmail.com>
2016-04-23 13:44:12 -0600

    add trip model tests for untested methods

--- space_age/spec/models/trip_spec.rb
+++ space_age/spec/models/trip_spec.rb
@@ -16,6 +16,44 @@ RSpec.describe Trip, type: :model do
     trip = Trip.new({"1" => 1, "2" => 2})
 
    expect(trip.total_price).to eq(5000)
-
   end
+
+  it "can get total quantity of packages" do
+    trip = Trip.new({"1" => 1, "2" => 2})
+
+    expect(trip.quantity).to eq 3
+  end
+
+  it "can get sub_quantity of an individual package" do
+    package1 = Package.create(id: 1)
+    package1.save(validates: false)
+    package2 = Package.create(id: 2)
+    package2.save(validates: false)
+    trip = Trip.new({"1" => 1, "2" => 2})
+
+    expect(trip.sub_quantity(2)).to eq 2
+  end
+
+  it "can adjust quantity to an individual package" do
+    trip = Trip.new({"1" => 1, "2" => 2})
+
+    trip.adjust_quantity("+", "1")
+
+    expect(trip.itinerary["1"]).to eq 2
+  end
+
+  it "can get sub_total of an individual package" do
+    package1 = Package.create(id: 1, price: 2000)
+    package1.save(validates: false)
+    package2 = Package.create(id: 2, price: 3000)
+    package2.save(validates: false)
+    trip = Trip.new({"1" => 1, "2" => 2})
+
+
+    expect(trip.subtotal(package1)).to eq 2000.0
+    expect(trip.subtotal(package2)).to eq 6000.0
+  end
+
+
+
 end
