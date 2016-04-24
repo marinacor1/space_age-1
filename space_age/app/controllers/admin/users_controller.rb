85e51de9f7c73743ec6a147ef13742ea2a9d16a2
Jeneve Parrish <jeneve.parrish@gmail.com>
2016-04-23 22:07:46 -0600

    adds functionality to the admins dashboard

--- space_age/app/controllers/admin/users_controller.rb
+++ space_age/app/controllers/admin/users_controller.rb
@@ -2,6 +2,9 @@ class Admin::UsersController < Admin::BaseController
   # before_action: require_login!, except: [:new, :creaet]
   def show
     @admin = current_user
+    @destination = Destination.new
+    @package = Package.new
+    @destinations = Destination.all
   end
-  
+
 end
