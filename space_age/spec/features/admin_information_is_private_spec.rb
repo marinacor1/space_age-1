72f66153cb33b770365174657b0c9097cd1f7915
marinacor1 <marinaspam1234@gmail.com>
2016-04-24 16:18:01 -0600

    Feature test written for admin information is private

---
+++ space_age/spec/features/admin_information_is_private_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+
+RSpec.feature "admin information is private" do
+  include FeatureHelper
+  it "registered user does not have admin access" do
+    @user = User.create(username: "Sam", email: "email1", password: "password", password_confirmation: "password")
+    admin = User.create(username: "adminor", email: "emailzzz", password: "password", password_confirmation: "password", role: 1)
+
+    user_login
+
+    visit '/admin/dashboard'
+
+    expect(page).to have_content "This site is not available."
+
+    visit '/admin/users/1'
+
+    expect(page).to have_content "This site is not available."
+  end
+end
