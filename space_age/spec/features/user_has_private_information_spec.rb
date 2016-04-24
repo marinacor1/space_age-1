3bada68e84227e310291fc849299f0de7b83d60d
marinacor1 <marinaspam1234@gmail.com>
2016-04-24 17:49:53 -0600

    guest cannot checkout spec passing

--- space_age/spec/features/user_has_private_information_spec.rb
+++ space_age/spec/features/user_has_private_information_spec.rb
@@ -21,4 +21,19 @@ RSpec.feature "user cannot see another users private data" do
     expect(page).to_not have_content package2.title
     expect(page).to_not have_content order.total_cost
   end
+
+  it "guest cannot see user information" do
+    user2 = User.create(username: "Miguel", email: "email277k", password: "password", password_confirmation: "password")
+
+    order = Order.create(user_id: user2.id, total_cost: 3000)
+    package1, package2 = create_list(:package, 2)
+    OrderPackage.create(order_id: order.id, user_id: user2.id,
+                       package_id: package1.id)
+    OrderPackage.create(order_id: order.id, user_id: user2.id,
+                       package_id: package2.id)
+
+    visit order_path(order)
+
+    expect(page).to have_content "The page you were looking for doesn't exist."
+  end
 end
