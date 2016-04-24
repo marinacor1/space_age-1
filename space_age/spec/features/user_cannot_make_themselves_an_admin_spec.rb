18fcad147c4a0fc3279d18d36367c86dfea0bca3
marinacor1 <marinaspam1234@gmail.com>
2016-04-24 17:39:57 -0600

    user cannot make themselves an admin

--- space_age/spec/features/user_cannot_make_themselves_an_admin_spec.rb
+++ space_age/spec/features/user_cannot_make_themselves_an_admin_spec.rb
@@ -7,5 +7,8 @@ RSpec.feature "user cannot make themselves an admin" do
 
     user_login
 
+    visit '/admin/dashboard'
+
+    expect(page).to have_content "The page you were looking for doesn't exist"
     end
 end
