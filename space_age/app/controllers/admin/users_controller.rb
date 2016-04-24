d608fbad38d32c6d1a7b88d84c5a1f705f836aaa
Jeneve Parrish <jeneve.parrish@gmail.com>
2016-04-24 10:52:57 -0600

    cleaning up

--- space_age/app/controllers/admin/users_controller.rb
+++ space_age/app/controllers/admin/users_controller.rb
@@ -1,7 +1,7 @@
 class Admin::UsersController < Admin::BaseController
   # before_action: require_login!, except: [:new, :creaet]
   def show
-    @admin = current_user
+    @user = current_user
     @destination = Destination.new
     @package = Package.new
     @destinations = Destination.all
