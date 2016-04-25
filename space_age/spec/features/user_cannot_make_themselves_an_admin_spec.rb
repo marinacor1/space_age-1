f7e62031e50b4f3fe4bd501e45aabdcc0a4ba52c
marinacor1 <marinaspam1234@gmail.com>
2016-04-25 09:12:19 -0600

    Stubbing in user cannot make themselves admin

--- space_age/spec/features/user_cannot_make_themselves_an_admin_spec.rb
+++ space_age/spec/features/user_cannot_make_themselves_an_admin_spec.rb
@@ -1,11 +1,10 @@
 require 'rails_helper'
 
 RSpec.feature "user cannot make themselves an admin" do
-  include FeatureHelper
   it "user will have no access to params" do
-    @user = User.create(username: "Sam", email: "email123kk", password: "password", password_confirmation: "password")
+    user = User.create(username: "Sam", email: "email123kk", password: "password", password_confirmation: "password")
 
-    user_login
+    ApplicationController.any_instance.stubs(:current_user).returns(user)
 
     visit '/admin/dashboard'
 
