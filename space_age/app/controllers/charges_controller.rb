9e53f4b41f66de81e48f850386a739ea1505e310
Jeneve Parrish <jeneve.parrish@gmail.com>
2016-04-26 15:23:34 -0600

    Order is created from Stripe checkout

--- space_age/app/controllers/charges_controller.rb
+++ space_age/app/controllers/charges_controller.rb
@@ -4,7 +4,6 @@ class ChargesController < ApplicationController
   end
 
   def create
-
     @amount = @trip.total_price.to_i * 100
 
     customer = Stripe::Customer.create(
@@ -18,6 +17,13 @@ class ChargesController < ApplicationController
       :description => current_user.username,
       :currency    => 'usd'
     )
+    order = Order.create(user_id: current_user.id, total_cost: @trip.total_price, status: 1)
+    @trip.itinerary.keys.each do |id|
+      order.order_packages.create(user_id: current_user.id, order_id: order.id, package_id: id.to_i)
+    end
+    flash[:success] = "Order was Successfully Placed, You Paid #{@amount}"
+    session[:trip].clear
+    redirect_to orders_path
 
   rescue Stripe::CardError => e
     flash[:error] = e.message
