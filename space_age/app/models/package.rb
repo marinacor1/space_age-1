90a24901c90ce87b10a433a4f936ffee85669ac8
Jbern16 <jbern16@gmail.com>
2016-04-27 16:23:50 -0600

    push before deploy

--- space_age/app/models/package.rb
+++ space_age/app/models/package.rb
@@ -1,8 +1,8 @@
 class Package < ActiveRecord::Base
   belongs_to :destination
   has_attached_file :image
-  validates_attachment_content_type :image, content_type: ["image/jpg", "image/jpeg", "image/png"],
-                                    default_url: "missing.jpg"
+  validates_attachment_content_type :image, content_type: ["image/jpg", "image/jpeg", "image/png"]
+
   validates :title, uniqueness: true
   validates_presence_of :title, :description, :price
 
