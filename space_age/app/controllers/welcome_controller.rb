601a7f459153229c9eb3d44f8bc449e5992ab42c
marinacor1 <marinaspam1234@gmail.com>
2016-04-27 14:52:01 -0600

    Fixing semicolon in scss doc

--- space_age/app/controllers/welcome_controller.rb
+++ space_age/app/controllers/welcome_controller.rb
@@ -1,5 +1,5 @@
 class WelcomeController < ApplicationController
-  require 'json'
+  require "json"
 
   def show
     nasa_api = "https://api.nasa.gov/planetary/apod?api_key=NNKOjkoul8n1CH18TWA9gwngW1s1SmjESPjNoUFo"
