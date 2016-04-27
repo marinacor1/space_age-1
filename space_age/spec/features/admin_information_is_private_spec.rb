bf6a80fc3f9d8f1d60f70ad22763fdd04c4100b5
marinacor1 <marinaspam1234@gmail.com>
2016-04-27 13:26:17 -0600

    Fix spacing

--- space_age/spec/features/admin_information_is_private_spec.rb
+++ space_age/spec/features/admin_information_is_private_spec.rb
@@ -3,14 +3,14 @@ require 'rails_helper'
 RSpec.feature "admin information is private" do
   it "registered user does not have admin access" do
     user = User.create(username: "Sam",
-                          email: "email123kk",
+                       email: "email123kk",
                        password: "password",
-          password_confirmation: "password")
+                       password_confirmation: "password")
     User.create(username: "adminor",
-                   email: "emailzo",
+                email: "emailzo",
                 password: "password",
-   password_confirmation: "password",
-                    role: 1)
+                password_confirmation: "password",
+                role: 1)
 
     ApplicationController.any_instance.stubs(:current_user).returns(user)
 
