29b54601a28da4a36a09fac63e4502f357cc8017
marinacor1 <marinaspam1234@gmail.com>
2016-05-03 22:49:51 -0600

    adding delete logout route

--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,18 @@
 Rails.application.routes.draw do
   root to: "welcome#show"
-
-  get "/packages", to: "packages#index"
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
   get '/signup', to: 'users#new'
+  delete '/logout', to: 'sessions#destroy'
+
+  get "/packages", to: "packages#index"
+
   get '/logout', to: 'sessions#destroy'
   get '/trip', to: 'trip_packages#show'
   get '/orders', to: 'orders#index'
-  post '/signup', to: 'users#create'
   get '/dashboard', to: 'users#show'
   post '/users', to: 'users#create'
 
-  get '/logout', to: 'sessions#destroy'
-  get '/:planet', to: 'destinations#show', as: :destination
   get '/trip_packages', to: 'trip_packages#show'
   post '/trip_packages', to: 'trip_packages#show'
   patch '/trip_package', to: 'trip_packages#update'
@@ -36,4 +35,8 @@ Rails.application.routes.draw do
   resources :users, only: [:update]
   post '/charges', to: 'charges#create'
   get '/charges/new', to: 'charges#create'
+
+  get '/:planet', to: 'destinations#show', as: :destination
+  get '/destinations', to: 'destinations#create'
+
 end
