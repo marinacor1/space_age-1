6669e92d308ac3597d3f641582d8f1a7ff0e3feb
marinacor1 <marinaspam1234@gmail.com>
2016-05-03 21:56:27 -0600

    adding order routes

--- config/routes.rb
+++ config/routes.rb
@@ -16,9 +16,12 @@ Rails.application.routes.draw do
   get '/:planet', to: 'destinations#show', as: :destination
   get '/trip_packages', to: 'trip_packages#show'
   post '/trip_packages', to: 'trip_packages#show'
+  # get '/orders/:id', to: 'orders#show'
+  get '/orders', to: 'orders#index'
   # get '/packages/:id', to: 'packages#show'
   # patch '/users/:id', to: 'users#update'
   # put '/users/:id', to: 'users#update'
+  resources :orders, only: [:show, :create]
   resources :packages, only: [:index, :show, :create]
   resources :users, only: [:create, :update]
 end
