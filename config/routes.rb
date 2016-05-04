3594567704593e4c5c2bad33c1fae495b9c25988
marinacor1 <marinaspam1234@gmail.com>
2016-05-03 22:53:32 -0600

    Reorganizing routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,15 +3,25 @@ Rails.application.routes.draw do
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
   get '/signup', to: 'users#new'
+  post '/signup', to: 'users#create'
   delete '/logout', to: 'sessions#destroy'
 
+  post '/users', to: 'users#create'
+  resources :users, only: [:update]
+  get '/dashboard', to: 'users#show'
+
+  namespace :admin do
+    get '/dashboard', to: 'users#show'
+    resources :users, only: [:update, :delete]
+    get '/orders', to: 'orders#index'
+  end
+  get '/trip', to: 'trip_packages#show'
+  post '/charges', to: 'charges#create'
+  get '/charges/new', to: 'charges#new'
   get "/packages", to: "packages#index"
 
   get '/logout', to: 'sessions#destroy'
-  get '/trip', to: 'trip_packages#show'
   get '/orders', to: 'orders#index'
-  get '/dashboard', to: 'users#show'
-  post '/users', to: 'users#create'
 
   get '/trip_packages', to: 'trip_packages#show'
   post '/trip_packages', to: 'trip_packages#show'
@@ -19,11 +29,6 @@ Rails.application.routes.draw do
   # get '/orders/:id', to: 'orders#show'
   get '/orders', to: 'orders#index'
 
-  namespace :admin do
-    get '/dashboard', to: 'users#show'
-    resources :users, only: [:update, :delete]
-    get '/orders', to: 'orders#index'
-  end
   # get '/packages/:id', to: 'packages#show'
   # patch '/users/:id', to: 'users#update'
   # put '/users/:id', to: 'users#update'
@@ -31,10 +36,6 @@ Rails.application.routes.draw do
   resources :orders, only: [:show]
   post '/packages', to: 'packages#create'
   resources :packages, only: [ :show]
-  post '/signup', to: 'users#create'
-  resources :users, only: [:update]
-  post '/charges', to: 'charges#create'
-  get '/charges/new', to: 'charges#create'
 
   get '/:planet', to: 'destinations#show', as: :destination
   get '/destinations', to: 'destinations#create'
