5bdedec17d53a5345baf10c7e327975a76fbe0fd
marinacor1 <marinaspam1234@gmail.com>
2016-05-03 21:15:33 -0600

    Adding initial routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,30 +1,17 @@
 Rails.application.routes.draw do
-
   root to: "welcome#show"
-  get '/login', to: "sessions#new"
-  post '/login', to: "sessions#create"
-  get '/signup', to: "users#new"
 
-  delete '/logout', to: 'sessions#destroy'
+  get "/packages", to: "packages#index"
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  get '/signup', to: 'users#new'
 
-  resources :users, only: [:create, :update]
-  get '/dashboard', to: "users#show"
+  get '/trip', to: 'trip_packages#show'
+  get '/orders', to: 'orders#index'
+  post '/signup', to: 'users#create'
+  get '/dashboard', to: 'users#show'
+  post '/users', to: 'users#create'
 
-  namespace :admin do
-    get '/dashboard', to: "users#show"
-    resources :users, only: [:update, :delete]
-    get '/orders' , to: "orders#index"
-  end
-
-  get '/trip', to: "trip_packages#show"
-  resources :charges, only: [:new, :create]
-
-  resources :packages, only: [:index, :show, :create]
-  resources :trip_packages, only: [:create, :destroy, :update]
-  resources :orders, only: [:show, :create]
-
-  get '/orders' , to: "orders#index"
-
-  get '/:planet', to: "destinations#show", as: :destination
-  resources :destinations, only: [:create]
+  get '/logout', to: 'destroy#users'
+  patch '/users/:id', to: 'users#update'
 end
